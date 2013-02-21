89049105edd750017cd5871b9d41543615639a9a
Andreas Finger <webmaster@mediafinger.com>
2013-02-21 23:47:59 +0100

    Don't log the stupid asset shit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,10 @@ Contractor::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
+
+  # Do not show assets in log
+  config.assets.logger = false
 
   # Set generators
   config.generators do |g|
