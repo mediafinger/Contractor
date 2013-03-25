d3ae726f7bdf5262dba28c5b02ca0cf9a465365b
Andreas Finger <webmaster@mediafinger.com>
2013-03-25 01:04:41 +0100

    Update Mail Spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,17 @@ require "spec_helper"
 
 describe UserMailer do
   describe "signup_confirmation" do
-    let(:mail) { UserMailer.signup_confirmation }
+    let(:user) { User.create(name: "willi", email: "wonker@example.com") }
+    let(:mail) { UserMailer.signup_confirmation(user) }
 
     it "renders the headers" do
       mail.subject.should eq("Signup confirmation")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.to.should eq([user.email])
+      mail.from.should eq(["info@onosono.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("UserMailer#signup_confirmation\r\nThanks for signing up to Contractor, #{user.name}!\r\n")
     end
   end
 
