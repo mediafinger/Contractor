d8187c9647de7048a85417e78b56f8833a346eec
Andreas Finger <andreas.finger@xing.com>
2013-04-10 11:49:13 +0200

    Removed touch => true temporarily

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ActiveRecord::Base
-  belongs_to :owner, :polymorphic => true, :touch => true
+  belongs_to :owner, :polymorphic => true
+  #TODO: figure out how to implement this for Rails 4:    , :touch => true
+  #HINT: try this gem https://github.com/rails/cache_digests on Rails 3 branch
 
   validates :owner_id,   :presence => true, :uniqueness => { :scope => :owner_type }
   validates :owner_type, :presence => true
